09094cbafe7eb976c8ff41508d9afc575fe9b01c
unknown <hudtakim@gmail.com>
2025-05-19 15:57:10 +0700

    adding post routes

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello world')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/") 
+def root():
+    return {
+        "Hello" : "World"
+    }
+
+items = []
+
+@app.post("/items") 
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}") 
+def create_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
